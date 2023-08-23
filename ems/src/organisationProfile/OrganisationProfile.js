fb84fb9614930e4a693186d141318212adf488e5
SARAN-S-M <saran007mgp@gmail.com>
2023-08-23 21:05:44 +0530

    Auth User Count Bug Solved

--- ems/src/organisationProfile/OrganisationProfile.js
+++ ems/src/organisationProfile/OrganisationProfile.js
@@ -186,7 +186,8 @@ const OrganisationProfile = () => {
             }
         }
         const getAuthUserCount = async () =>{
-            const response = await axios.get(`${API_URL}/organisation/authUserCount`, {
+            try
+            {const response = await axios.get(`${API_URL}/organisation/authUserCount`, {
                 headers: {
                     Authorization: `Bearer ${Cookies.get('authToken')}`,
                     'Bypass-Tunnel-Reminder': 'eventaz',
@@ -196,8 +197,13 @@ const OrganisationProfile = () => {
             if (response.status === 200){
                 setAuthUserCount(response.data.authUserCount);
             }
-
             else{
+                alert('Error fetching data. Please try again later.');
+            }} catch(error) {
+                if(error.response.status === 400){
+                    setAuthUserCount(0);
+                    return;
+                }
                 alert('Error fetching data. Please try again later.');
             }
         };
